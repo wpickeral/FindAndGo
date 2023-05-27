e6d13b69d7392eed0640a34d37ecf3b51afee6e1
wpickeral <willpickeral@gmail.com>
2023-05-26 23:33:51 -0400

    remove errors from the LocationController.cs

--- FindAndGo/Controllers/LocationController.cs
+++ FindAndGo/Controllers/LocationController.cs
@@ -2,6 +2,4 @@ namespace FindAndGo.Controllers;
 
 public class LocationController
 {
-    var apiKey = Environment.GetEnvironmentVariable("WEATHER_API_KEY");
-    var weather = new OpenWeatherMapAPI(new HttpClient(), apiKey);
 }
