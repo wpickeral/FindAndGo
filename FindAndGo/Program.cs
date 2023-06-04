4a73c980c53726e0e2b5a9d0c6ccfe0b2c82c019
wpickeral <willpickeral@gmail.com>
2023-06-04 14:02:04 -0400

    fix the dollar sign issue

--- FindAndGo/Program.cs
+++ FindAndGo/Program.cs
@@ -8,14 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient<IKrogerService, KrogerService>();
 
-builder.Services.Configure<RequestLocalizationOptions>(options =>
-{
-    var supportedCultures = new[] { "en-US" };
-    options.SetDefaultCulture(supportedCultures[0])
-        .AddSupportedCultures(supportedCultures)
-        .AddSupportedUICultures(supportedCultures);
-});
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
