d3b09be992d57027f017f8f1b4838c0b7161e53f
wpickeral <willpickeral@gmail.com>
2023-06-13 23:41:35 -0400

    Removed conditional to check for the token from the middleware class and implemented the UseWhen method.

--- FindAndGo/Middleware/KrogerMiddleware.cs
+++ FindAndGo/Middleware/KrogerMiddleware.cs
@@ -14,33 +14,28 @@ public class KrogerMiddleware
     // IKrogerService is injected into InvokeAsync
     public async Task InvokeAsync(HttpContext httpContext, IKrogerService krogerService)
     {
-        var token = httpContext.Request.Cookies["find-and-go.token"];
-
-        if (token == null)
+        try
         {
-            try
+            // Request a new token
+            var newTokenRequest = await krogerService.GetAccessToken();
+            if (newTokenRequest != null)
             {
-                // Request a new token
-                var newTokenRequest = await krogerService.GetAccessToken();
-                if (newTokenRequest != null)
-                {
-                    // Parse the access_token
-                    var newToken = newTokenRequest["access_token"].ToString();
-                    // Parse the MaxAge
-                    var expiresIn = int.Parse(newTokenRequest["expires_in"].ToString());
-                    // Build the cookie options object
-                    var cookieOptions = new CookieOptions();
-                    cookieOptions.Expires = DateTimeOffset.Now.AddSeconds(expiresIn);
-                    // Add the token to the cookies object with the options
-                    httpContext.Response.Cookies.Append("find-and-go.token", newToken, cookieOptions);
-                }
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine(e);
+                // Parse the access_token
+                var newToken = newTokenRequest["access_token"].ToString();
+                // Parse the MaxAge
+                var expiresIn = int.Parse(newTokenRequest["expires_in"].ToString());
+                // Build the cookie options object
+                var cookieOptions = new CookieOptions();
+                cookieOptions.Expires = DateTimeOffset.Now.AddSeconds(expiresIn);
+                // Add the token to the cookies object with the options
+                httpContext.Response.Cookies.Append("find-and-go.token", newToken, cookieOptions);
             }
         }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine(e);
+        }
 
-        await _next(httpContext);   
+        await _next(httpContext);
     }
 }
